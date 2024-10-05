85a913f6ea1cc7e9eff5aa9da7f57993507962fc
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-10-05 23:28:18 +0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/emodipt-extend.omp.json' | Invoke-Expression
