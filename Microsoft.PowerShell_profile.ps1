2e7be46461bd176e896487092d0fc5f0e42ec06d
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-10-07 17:47:45 +0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # Oh My Posh:
 oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubbles.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config ~\mnml.omp.json | Invoke-Expression
 
 # Auto Complete
 Set-PSReadLineOption -PredictionSource History
