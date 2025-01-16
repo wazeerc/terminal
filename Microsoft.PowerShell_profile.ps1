07f21258d3d84115fca639e488c323d49735f29a
Wazeer <58399263+wazeerc@users.noreply.github.com>
2025-01-16 16:20:14 +0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -28,3 +28,36 @@ function deldir {
 
 ## Grep Alternative for Windows
 Set-Alias find findstr
+
+## Git Aliases
+function Test-GitRepository {
+    try {
+        if (-not (Test-Path -Path ".git")) {
+            Write-Error "You're not in a Git Repository! 🤬" -ErrorAction Stop
+            return $false
+        }
+        return $true
+    }
+    catch {
+        Write-Host "Error: $_"
+        return $false
+    }
+}
+
+### Git status
+function git-status {
+    if (Test-GitRepository) { git status }
+}
+Set-Alias -Name gs -Value git-status
+
+### Git show one line, five most recent commits
+function git-log5 {
+    if (Test-GitRepository) { git log -n 5 --oneline }
+}
+Set-Alias -Name g5 -Value git-log5
+
+### Git rebase main
+function git-rebase-main {
+    if (Test-GitRepository) { git rebase main }
+}
+Set-Alias -Name grm -Value git-rebase-main
